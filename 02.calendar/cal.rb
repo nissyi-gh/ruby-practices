3bb602485c5c12d1e7f6ecd179029734522a3529
nissyi <yuta.onishi@pepabo.com>
2022-06-07 17:50:52 +0900

    不要なコメントを削除
    処理の内容が一目でわかるコメントは削除しました

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,22 +1,16 @@
 require 'date'
 require 'optparse'
 
-# 本日の指定、比較用にDateオブジェクトを生成
 today = Date.today
-# OptionParserオブジェクトを生成
 opt = OptionParser.new
-# 取り扱うOptionを設定
 opt.on('-m')
 opt.on('-y')
-# ハッシュに引数を格納する
 params = ARGV.getopts("", "y:#{today.year}", "m:#{today.month}")
 
-# オプションの年を検証して、無効ならエラー文を表示する
 def validation_of_year(year)
   puts "Invalid year." unless year >= 1 && year <= 9999
 end
 
-# オプションの月を検証して、無効ならエラー文を表示する
 def validation_of_month(month)
   puts "Invalid month." unless month >= 1 && month <= 12
 end
