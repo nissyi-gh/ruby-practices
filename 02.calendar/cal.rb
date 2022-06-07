3c49d9ad0a6158f50bcf5a850e48f8e4f60771d5
nissyi <yuta.onishi@pepabo.com>
2022-06-07 13:08:24 +0900

    年月のvalidationを作成
    無効な入力がされていた場合、エラーを表示する処理を作成しました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,4 +10,18 @@ opt.on('-m')
 opt.on('-y')
 # ハッシュに引数を格納する
 params = ARGV.getopts("", "y:#{today.year}", "m:#{today.month}")
+
+# オプションの年を検証して、無効ならエラー文を表示する
+def validation_of_year(year)
+  puts "Invalid year." unless year >= 1 && year <= 9999
+end
+
+# オプションの月を検証して、無効ならエラー文を表示する
+def validation_of_month(month)
+  puts "Invalid month." unless month >= 1 && month <= 12
+end
+
+validation_of_year(params["y"] = params["y"].to_i)
+validation_of_month(params["m"] = params["m"].to_i)
+
 p params
