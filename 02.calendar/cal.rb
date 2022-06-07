76ebc6c1aaec5e6bdb43f054053bb3f8e2d9844e
nissyi <yuta.onishi@pepabo.com>
2022-06-07 12:19:39 +0900

    年月のデフォルト値を設定
    オプションが入力されていないときには、本日の年月を格納するように変更しました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,11 +1,13 @@
 require 'date'
 require 'optparse'
 
+# 本日の指定、比較用にDateオブジェクトを生成
+today = Date.today
 # OptionParserオブジェクトを生成
 opt = OptionParser.new
 # 取り扱うOptionを設定
 opt.on('-m')
 opt.on('-y')
 # ハッシュに引数を格納する
-params = ARGV.getopts("y:m:")
-# p params
+params = ARGV.getopts("", "y:#{today.year}", "m:#{today.month}")
+p params
