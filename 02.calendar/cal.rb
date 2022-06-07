0b5f9d02a3974ed453a889be1f5a23ea14084ce7
nissyi <yuta.onishi@pepabo.com>
2022-06-07 12:05:13 +0900

    -y, -mオプションを取得
    カレンダーの年月を指定するためにオプションを取得できるようにしました。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,2 +1,11 @@
 require 'date'
 require 'optparse'
+
+# OptionParserオブジェクトを生成
+opt = OptionParser.new
+# 取り扱うOptionを設定
+opt.on('-m')
+opt.on('-y')
+# ハッシュに引数を格納する
+params = ARGV.getopts("y:m:")
+# p params
