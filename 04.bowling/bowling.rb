7186089f9da850500ba734473932d2a89d36ff8c
nissyi <yuta.onishi@pepabo.com>
2022-06-09 19:15:12 +0900

    フレームごとへの分割をメソッド化
    可読性向上のためメソッド化を行いました

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -15,17 +15,24 @@ def replace_strike_to_score_and_convert_integer(score)
   shots
 end
 
+def slice_each_frame(shots)
+  frames = []
+
+  shots.each_slice(2) do |s|
+    frames << s
+  end
+
+  if frames[10]
+    frames[9].concat(frames[10])
+    frames.pop
+  end
+
+  frames
+end
+
 score = ARGV[0].split(",")
 shots = []
 frames = []
 
 shots = replace_strike_to_score_and_convert_integer(score)
-
-shots.each_slice(2) do |s|
-  frames << s
-end
-
-if frames[10]
-  frames[9].concat(frames[10])
-  frames.pop
-end
+frames = slice_each_frame(shots)
