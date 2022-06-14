e77d3ac88f243aef3eeed4659b1508156cce14c8
nissyi <yuta.onishi@pepabo.com>
2022-06-14 10:30:39 +0900

    シェルスクリプトに実行したコマンドの内容を表示するように変更
    実行結果を分かりやすくするために修正しました。

--- 04.bowling/test.sh
+++ 04.bowling/test.sh
@@ -12,26 +12,38 @@ function result_check() {
   echo "-------"
 }
 
-result=`ruby ./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5`
+command="./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5"
+echo "command: ${command}"
+result=`ruby ${command}`
 expect=139
 result_check ${expect} ${result}
 
-result=`ruby ./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X`
+command="./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X"
+echo "command: ${command}"
+result=`ruby ${command}`
 expect=164
 result_check ${expect} ${result}
 
-result=`ruby ./bowling.rb 0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4`
+command="./bowling.rb 0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4"
+echo "command: ${command}"
+result=`ruby ${command}`
 expect=107
 result_check ${expect} ${result}
 
-result=`ruby ./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0`
+command="./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0"
+echo "command: ${command}"
+result=`ruby ${command}`
 expect=134
 result_check ${expect} ${result}
 
-result=`ruby ./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8`
+command="./bowling.rb 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8"
+echo "command: ${command}"
+result=`ruby ${command}`
 expect=144
 result_check ${expect} ${result}
 
-result=`ruby ./bowling.rb X,X,X,X,X,X,X,X,X,X,X,X`
+command="./bowling.rb X,X,X,X,X,X,X,X,X,X,X,X"
+echo "command: ${command}"
+result=`ruby ${command}`
 expect=300
 result_check ${expect} ${result}
