6a3c7baa86f44e476e0d1861067e437f30ef852d
nissyi <yuta.onishi@pepabo.com>
2022-06-20 16:35:14 +0900

    テスト内容の修正
    テスト対象とするテキストの内容が誤っていたため修正しました。

--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -3,6 +3,6 @@ require_relative './ls'
 
 class LsTest < Minitest::Test
   def test_without_option
-    assert_equal ".gitkeep\nt.txt\nls.rb", ls
+    assert_equal "ls.rb ls_test.rb", ls
   end
 end
