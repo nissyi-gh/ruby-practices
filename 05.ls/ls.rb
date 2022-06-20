4380222f1472e7daeb2536c073076badeb58bb38
nissyi <yuta.onishi@pepabo.com>
2022-06-20 18:21:37 +0900

    コマンドラインからディレクトリの指定を受け取れるように変更
    ..を受け取るために変更し、入力がなければカレントディレクトリを表示するようにしました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,4 +1,4 @@
-def no_option(path_name='.')
+def no_option(path_name)
   file_names = []
   Dir.children(path_name).each do |file_name|
     file_names << file_name unless file_name[0] == '.'
@@ -7,4 +7,5 @@ def no_option(path_name='.')
   file_names.sort.join(' ')
 end
 
-no_option
+path_name = ARGV[0] || '.'
+puts no_option(path_name)
