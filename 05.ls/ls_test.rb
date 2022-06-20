9d92798d28ebfd32b8c9a63d8b45e52438819ae9
nissyi <yuta.onishi@pepabo.com>
2022-06-20 17:54:57 +0900

    testで呼び出すメソッド名の修正
    メソッド名をlsからno_optionに変更したため当該箇所を修正しました。

--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -3,6 +3,6 @@ require_relative './ls'
 
 class LsTest < Minitest::Test
   def test_without_option
-    assert_equal "ls.rb ls_test.rb", ls
+    assert_equal "ls.rb ls_test.rb", no_option
   end
 end
