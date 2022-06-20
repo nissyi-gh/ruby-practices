93615f61f20ee5947c872cdddcbe052802bbf3fd
nissyi <yuta.onishi@pepabo.com>
2022-06-20 19:46:12 +0900

    指定した幅で表示する機能を実装
    定数の値を変更することで、表示の幅を変更できるようにしました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,11 +1,31 @@
+LIST_WIDTH = 3
+
 def without_option(path_name)
   file_names = []
   Dir.children(path_name).each do |file_name|
     file_names << file_name unless file_name[0] == '.'
   end
 
-  file_names.sort.join(' ')
+  sort_file_names = file_names.sort
+  outputs = []
+  rows = []
+  list_height = (file_names.size.to_f / LIST_WIDTH).ceil
+
+  sort_file_names.each do |file_name|
+    rows << file_name
+    if rows.size % list_height == 0 || sort_file_names.last == file_name
+      outputs << rows
+      rows = []
+    end
+  end
+
+  list_height.times do |n|
+    outputs.each do |output|
+      print "#{output[n]} " if output[n]
+    end
+    puts
+  end
 end
 
 path_name = ARGV[0] || '.'
-print without_option(path_name)
+without_option(path_name)
