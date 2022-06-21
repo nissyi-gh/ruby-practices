f9eb21c3c8cf7c04da48bf665f25e4692841ec01
nissyi <yuta.onishi@pepabo.com>
2022-06-21 12:36:45 +0900

    RuboCop対応
    RuboCopにより修正が必要な箇所の修正を行いました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'io/console/size'
 require 'pathname'
 LIST_COLUMNS = 3
@@ -47,9 +49,7 @@ end
 def configure_output_columns(file_name_width, console_width)
   output_columns = LIST_COLUMNS
 
-  until file_name_width * LIST_COLUMNS < console_width || output_columns == 1
-    output_columns -= 1
-  end
+  output_columns -= 1 until file_name_width * LIST_COLUMNS < console_width || output_columns == 1
 
   output_columns
 end
@@ -60,11 +60,11 @@ def format_file_names(sort_file_names, list_height)
 
   sort_file_names.each do |file_name|
     column << file_name
-    if column.size == list_height || sort_file_names.last == file_name
-      outputs << column
-      # column.clearをするとoutputsに入れた中身自体が消えてしまう
-      column = []
-    end
+    next if column.size != list_height && sort_file_names.last != file_name
+
+    outputs << column
+    # column.clearをするとoutputsに入れた中身自体が消えてしまう
+    column = []
   end
 
   outputs
@@ -73,7 +73,7 @@ end
 def print_file_names(list_height, output_style_file_names, file_name_width)
   list_height.times do |n|
     output_style_file_names.each do |file_name|
-      print "#{file_name[n].ljust(file_name_width + 2)}" if file_name[n]
+      print file_name[n].ljust(file_name_width + 2).to_s if file_name[n]
     end
     puts
   end
