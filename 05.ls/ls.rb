9a91dbce9d04e68b55eb8fa57dc0b16c46ea96ae
nissyi <yuta.onishi@pepabo.com>
2022-06-21 11:43:59 +0900

    ファイル名一覧と出力処理のメソッド化
    メインの処理から責務を分割するためにメソッド化を行いました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,28 +3,14 @@ LIST_COLUMNS = 3
 
 def without_option(path_name)
   file_names = load_file_names(path_name)
-  sort_file_names = file_names.sort
   console_width = IO.console_size[1]
-  outputs = []
-  rows = []
+
   file_name_width = configure_file_name_width(file_names)
   output_columns = configure_output_columns(file_name_width, console_width)
   list_height = (file_names.size.to_f / output_columns).ceil
 
-  sort_file_names.each do |file_name|
-    rows << file_name
-    if rows.size % list_height == 0 || sort_file_names.last == file_name
-      outputs << rows
-      rows = []
-    end
-  end
-
-  list_height.times do |n|
-    outputs.each do |output|
-      print "#{output[n].ljust(file_name_width + 2)}" if output[n]
-    end
-    puts
-  end
+  output_style_file_names = format_file_names(file_names.sort, list_height)
+  print_file_names(list_height, output_style_file_names, file_name_width)
 end
 
 def load_file_names(path_name)
@@ -53,6 +39,31 @@ def configure_output_columns(file_name_width, console_width)
   end
 
   output_columns
+end
+
+def format_file_names(sort_file_names, list_height)
+  column = []
+  outputs = []
+
+  sort_file_names.each do |file_name|
+    column << file_name
+    if column.size == list_height || sort_file_names.last == file_name
+      outputs << column
+      # column.clearをするとoutputsに入れた中身自体が消えてしまう
+      column = []
+    end
+  end
+
+  outputs
+end
+
+def print_file_names(list_height, output_style_file_names, file_name_width)
+  list_height.times do |n|
+    output_style_file_names.each do |file_name|
+      print "#{file_name[n].ljust(file_name_width + 2)}" if file_name[n]
+    end
+    puts
+  end
 end
 
 path_name = ARGV[0] || '.'
