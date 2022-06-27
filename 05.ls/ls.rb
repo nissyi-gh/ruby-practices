15070df978d64fea68849c5ce88f9bf3b99ef4a9
nissyi <yuta.onishi@pepabo.com>
2022-06-27 10:40:59 +0900

    sort処理を削除
    Dir.globでsortされるため削除しました

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -15,7 +15,7 @@ def without_option(path)
   output_columns = configure_output_columns(file_name_width, console_width)
   list_height = (file_names.size.to_f / output_columns).ceil
 
-  output_style_file_names = format_file_names(file_names.sort, list_height)
+  output_style_file_names = format_file_names(file_names, list_height)
   print_file_names(list_height, output_style_file_names, file_name_width)
 end
 
@@ -40,13 +40,13 @@ def configure_output_columns(file_name_width, console_width)
   output_columns
 end
 
-def format_file_names(sort_file_names, list_height)
+def format_file_names(file_names, list_height)
   column = []
   outputs = []
 
-  sort_file_names.each do |file_name|
+  file_names.each do |file_name|
     column << file_name
-    next if column.size != list_height && sort_file_names.last != file_name
+    next if column.size != list_height && file_names.last != file_name
 
     outputs << column
     # column.clearをするとoutputsに入れた中身自体が消えてしまう
