aa26446ffd7a1f08e3b96a6cd9425fa1beb87bcb
nissyi <yuta.onishi@pepabo.com>
2022-06-28 18:26:17 +0900

    変数の定数化と処理のDRY化
    DRYになっていなかった箇所の修正を行いました。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -4,50 +4,16 @@ require 'io/console/size'
 require 'pathname'
 require 'optparse'
 DEFAULT_COLUMN_COUNT = 3
+CONSOLE_WIDTH = IO.console_size[1]
 
 
 def simulate_ls_command(path_names, params)
-  if params[:a]
-    with_all_option(path_names)
-  else
-    without_option(path_names)
-  end
-end
-
-def without_option(path_names)
-  console_width = IO.console_size[1]
-
-  path_names.each_with_index do |path_name, index|
-    puts "#{path_name}:" if path_names.size > 1
-
-    file_names = load_file_names(path_name)
-
+  path_names.each do |path_name|
+    file_names = params[:a] ? Dir.entries(path_name).sort : Dir.glob('*', base: path_name)
     next if file_names.empty?
 
-    file_name_width = file_names.map(&:length).max
-    column_count = configure_column_count(file_name_width, console_width)
-    list_height = (file_names.size.to_f / column_count).ceil
-    output_style_file_names = format_file_names(file_names, list_height)
-    print_file_names(list_height, output_style_file_names, file_name_width)
-  end
-end
-
-def with_all_option(path_names)
-  console_width = IO.console_size[1]
-
-  path_names.each_with_index do |path_name, index|
     puts "#{path_name}:" if path_names.size > 1
-
-    file_names = Dir.entries(path_name).sort
-
-    next if file_names.empty?
-
-    file_name_width = file_names.map(&:length).max
-    column_count = configure_column_count(file_name_width, console_width)
-    list_height = (file_names.size.to_f / column_count).ceil
-    output_style_file_names = format_file_names(file_names, list_height)
-    print_file_names(list_height, output_style_file_names, file_name_width)
-
+    print_file_names(file_names)
     puts unless path_name == path_names.last
   end
 end
@@ -77,19 +43,10 @@ def parse_path
   path_names.sort
 end
 
-def valid_path_name?(path_name)
-  path_name.exist?
-end
-
-def load_file_names(path_name, flags = 0)
-  # この先 ls -aなどで隠しファイルも表示できるようflagsを受け取れるようにしておく
-  Dir.glob('*', flags, base: path_name)
-end
-
-def configure_column_count(file_name_width, console_width)
+def configure_column_count(file_name_width)
   column_count = DEFAULT_COLUMN_COUNT
 
-  column_count -= 1 while file_name_width * DEFAULT_COLUMN_COUNT > console_width && column_count > 1
+  column_count -= 1 while file_name_width * DEFAULT_COLUMN_COUNT > CONSOLE_WIDTH && column_count > 1
 
   column_count
 end
@@ -110,7 +67,12 @@ def format_file_names(file_names, list_height)
   outputs
 end
 
-def print_file_names(list_height, output_style_file_names, file_name_width)
+def print_file_names(file_names)
+  file_name_width = file_names.map(&:length).max
+  column_count = configure_column_count(file_name_width)
+  list_height = (file_names.size.to_f / column_count).ceil
+  output_style_file_names = format_file_names(file_names, list_height)
+
   list_height.times do |n|
     output_style_file_names.each do |file_name|
       print file_name[n].ljust(file_name_width + 2).to_s if file_name[n]
