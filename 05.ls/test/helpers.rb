eee544b0ed4249815ba9d980f7443f981b3c7381
nissyi <yuta.onishi@pepabo.com>
2022-07-19 14:51:58 +0900

    オプションをセットするヘルパーを作成
    テストによってARGVにオプションがセットされていることを担保します。

---
+++ 05.ls/test/helpers.rb
@@ -0,0 +1,13 @@
+module TestHelpers
+  def set_a_option
+    ARGV << "-a"
+  end
+
+  def set_l_option
+    ARGV << '-l'
+  end
+
+  def set_r_option
+    ARGV << '-r'
+  end
+end
--- 05.ls/test/test.rb
+++ 05.ls/test/test.rb
@@ -1,9 +1,30 @@
-require 'minitest/unit'
 require 'minitest/autorun'
 require_relative '../ls'
+require_relative './helpers'
 
-class TestLs < MiniTest::Unit::TestCase
+class TestLs < MiniTest::Test
+  include TestHelpers
+
+  def setup
+    ARGV.clear
+  end
+
   def test_main
     assert_equal 'main', Ls.main
+  end
+
+  def test_a_option_is_set?
+    set_a_option
+    assert_equal ['-a'], ARGV
+  end
+
+  def test_l_option_is_set?
+    set_l_option
+    assert_equal ['-l'], ARGV
+  end
+
+  def test_r_option_is_set?
+    set_r_option
+    assert_equal ['-r'], ARGV
   end
 end
