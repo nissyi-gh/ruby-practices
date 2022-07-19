26abb626233cbfe5e72d0252c16ae1a0f87d4783
nissyi <yuta.onishi@pepabo.com>
2022-07-19 15:16:21 +0900

    オプションを設定するテストヘルパーを追加
    今回のケースであり得る15パターンのオプションを網羅できるようにします。

--- 05.ls/test/helpers.rb
+++ 05.ls/test/helpers.rb
@@ -10,4 +10,52 @@ module TestHelpers
   def set_r_option
     ARGV << '-r'
   end
+
+  def set_al_option
+    ARGV << '-al'
+  end
+
+  def set_ar_option
+    ARGV << '-ar'
+  end
+
+  def set_la_option
+    ARGV << '-la'
+  end
+
+  def set_lr_option
+    ARGV << '-lr'
+  end
+
+  def set_ra_option
+    ARGV << '-ra'
+  end
+
+  def set_rl_option
+    ARGV << '-rl'
+  end
+
+  def set_alr_option
+    ARGV << '-alr'
+  end
+
+  def set_arl_option
+    ARGV << '-arl'
+  end
+
+  def set_lar_option
+    ARGV << '-lar'
+  end
+
+  def set_lra_option
+    ARGV << '-lra'
+  end
+
+  def set_ral_option
+    ARGV << '-ral'
+  end
+
+  def set_rla_option
+    ARGV << '-rla'
+  end
 end
