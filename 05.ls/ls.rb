13a370d2de1bafcea99dea33d49c3f3ae862c585
nissyi <yuta.onishi@pepabo.com>
2022-07-19 19:49:11 +0900

    ファイル名を読み込む機能を実装
    lオプションはファイル以外のプロパティを読み込む必要があるため未実装です。

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,13 +6,20 @@ require 'pathname'
 class Ls
   @options = {}
   @path_names = []
+  @files = []
 
   class << self
     attr_reader :options, :path_names
+    attr_accessor :files
 
     def main
       parse_option
       parse_path
+
+      @path_names.each do |path_name|
+        load_files(path_name)
+        print_files
+      end
     end
 
     def parse_option
@@ -36,6 +43,21 @@ class Ls
             "ls: #{path_name}: No such file or directory"
           end
       end
+    end
+
+    def load_files(path_name)
+      @files =
+        if @options[:a]
+          Dir.entries(path_name).sort
+        else
+          Dir.glob('*', base: path_name)
+        end
+
+      @files.reverse! if @options[:r]
+    end
+
+    def print_files
+      puts @files
     end
 
     def clear_options
