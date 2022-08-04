5b6f8f0a7fa0ea3af72efbccccf66e934e9c2261
nissyi <yuta.onishi@pepabo.com>
2022-08-04 12:04:05 +0900

    ファイルのサイズとファイル名を表示できるようにする
    wcコマンドに必要な情報の一部を表示できるようにします。

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
+require 'pathname'
+
 def main
-  parse_paths
+  path_names = parse_paths
+
+  path_names.each do |path_name|
+    print File.size(path_name)
+    puts path_name
+  end
 end
 
 def parse_paths
+  path_names = []
+
   ARGV.each do |path|
     if !File.exist?(path)
       puts "wc: #{path}: open: No such file or directory"
     elsif File.directory?(path)
       puts "wc: #{path}: read: Is a directory"
+    else
+      path_names << Pathname.new(path)
     end
   end
+
+  path_names
 end
 
 main
