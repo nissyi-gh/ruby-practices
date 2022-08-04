a6c7e08b0e68af8fb27ec2e23fb4c106e9ca5a6c
nissyi <yuta.onishi@pepabo.com>
2022-08-04 11:02:14 +0900

    親ディレクトリ単体を指定した場合のテストを追加
    エラーのメッセージが出力されることをテストします。

--- 06.wc/test/test_wc.rb
+++ 06.wc/test/test_wc.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require_relative '../wc'
 
 class TestWc < MiniTest::Test
+  def test_specify_only_parent_directory
+    ARGV << '..'
+    assert_equal 'wc: ..: read: Is a directory', main
+  end
 end
