889a1af46b9664cacb5bf02b7fc9901a9e4fbae5
nissyi <yuta.onishi@pepabo.com>
2022-08-04 11:29:43 +0900

    存在しないディレクトリを指定した場合のテストを追加
    エラーメッセージが表示されるかテストします。

--- 06.wc/test/test_wc.rb
+++ 06.wc/test/test_wc.rb
@@ -8,4 +8,9 @@ class TestWc < MiniTest::Test
     ARGV << '..'
     assert_output ("wc: ..: read: Is a directory\n") { main }
   end
+
+  def test_specify_not_exist_directory
+    ARGV << './hogefugapiyo'
+    assert_output ("wc: ./hogefugapiyo: open: No such file or directory\n") { main }
+  end
 end
