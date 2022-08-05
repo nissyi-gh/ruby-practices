2b17a301987ae723b435af71a3ae1b66f7a2337f
nissyi <yuta.onishi@pepabo.com>
2022-08-05 14:26:50 +0900

    パイプで受け取った入力でも実行できるようにする
    パイプから入力を受け取れるようにしました

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,27 +7,36 @@ def main
   print_width = 8
   params = parse_options
 
-  ARGV.each do |path|
-    if !File.exist?(path)
-      puts "wc: #{path}: open: No such file or directory"
-    elsif File.directory?(path)
-      puts "wc: #{path}: read: Is a directory"
-    else
-      path_name = Pathname.new(path)
+  if ARGV.any?
+    ARGV.each do |path|
+      if !File.exist?(path)
+        puts "wc: #{path}: open: No such file or directory"
+      elsif File.directory?(path)
+        puts "wc: #{path}: read: Is a directory"
+      else
+        path_name = Pathname.new(path)
 
-      print parse_file_lines(path_name, params).to_s.rjust(print_width) if params[:l]
-      print parse_word_count(path_name, params).to_s.rjust(print_width) if params[:w]
-      print read_file_size(path_name, params).to_s.rjust(print_width) if params[:c]
-      print ' '
-      puts path_name
+        print parse_file_lines(path_name, params).to_s.rjust(print_width) if params[:l]
+        print parse_word_count(path_name, params).to_s.rjust(print_width) if params[:w]
+        print read_file_size(path_name, params).to_s.rjust(print_width) if params[:c]
+        print ' '
+        puts path_name
+      end
     end
-  end
 
-  if ARGV.size >= 2
-    print params[:l_total].to_s.rjust(print_width) if params[:l]
-    print params[:w_total].to_s.rjust(print_width) if params[:w]
-    print params[:c_total].to_s.rjust(print_width) if params[:c]
-    puts ' total'
+    if ARGV.size >= 2
+      print params[:l_total].to_s.rjust(print_width) if params[:l]
+      print params[:w_total].to_s.rjust(print_width) if params[:w]
+      print params[:c_total].to_s.rjust(print_width) if params[:c]
+      puts ' total'
+    end
+  else
+    stdin = $stdin
+    stdin = File.pipe?(stdin) ? stdin.to_a : stdin.readlines
+
+    print stdin.join.count("\n").to_s.rjust(print_width)
+    print stdin.size.to_s.rjust(print_width)
+    puts stdin.join.bytesize.to_s.rjust(print_width)
   end
 end
 
@@ -79,4 +88,4 @@ def read_file_size(path_name, params)
   file_size
 end
 
-main
+main if __FILE__ == $0
