5912ebba6a0f45111c4efb39033a9d97c9ec720c
nissyi <yuta.onishi@pepabo.com>
2022-08-05 16:11:22 +0900

    リファクタリング
    rubocopの規約違反を多数おこなっていたため修正します。

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,41 +3,32 @@
 require 'pathname'
 require 'optparse'
 
+PRINT_WIDTH = 8
+
 def main
-  print_width = 8
   params = parse_options
 
   if ARGV.any?
+    results = {}
+
     ARGV.each do |path|
-      if !File.exist?(path)
-        puts "wc: #{path}: open: No such file or directory"
-      elsif File.directory?(path)
-        puts "wc: #{path}: read: Is a directory"
-      else
-        path_name = Pathname.new(path)
-
-        print parse_file_lines(path_name, params).to_s.rjust(print_width) if params[:l]
-        print parse_word_count(path_name, params).to_s.rjust(print_width) if params[:w]
-        print read_file_size(path_name, params).to_s.rjust(print_width) if params[:c]
-        print ' '
-        puts path_name
-      end
+      results[path.to_sym] =
+        if !File.exist?(path)
+          "wc: #{path}: open: No such file or directory"
+        elsif File.directory?(path)
+          "wc: #{path}: read: Is a directory"
+        else
+          path_name = Pathname.new(path)
+          parse_from_path_name(path_name)
+        end
     end
 
-    if ARGV.size >= 2
-      print params[:l_total].to_s.rjust(print_width) if params[:l]
-      print params[:w_total].to_s.rjust(print_width) if params[:w]
-      print params[:c_total].to_s.rjust(print_width) if params[:c]
-      puts ' total'
-    end
+    print_from_path_names(results, params)
   else
     stdin = $stdin
     stdin = File.pipe?(stdin) ? stdin.to_a : stdin.readlines
 
-    print stdin.join.count("\n").to_s.rjust(print_width) if params[:l]
-    print stdin.size.to_s.rjust(print_width) if params[:w]
-    print stdin.join.bytesize.to_s.rjust(print_width) if params[:c]
-    puts
+    print_from_pipe_or_stdin(stdin, params)
   end
 end
 
@@ -45,48 +36,76 @@ def parse_options
   params = {}
 
   opt = OptionParser.new
-  opt.on('-l') do
-    params[:l] = true
-    params[:l_total] = 0
-  end
-  opt.on('-w') do
-    params[:w] = true
-    params[:w_total] = 0
-  end
-  opt.on('-c') do
-    params[:c] = true
-    params[:c_total] = 0
-  end
+  opt.on('-l') { params[:l] = true }
+  opt.on('-w') { params[:w] = true }
+  opt.on('-c') { params[:c] = true }
   opt.parse!(ARGV)
 
   params.empty? ? { l: true, l_total: 0, w: true, w_total: 0, c: true, c_total: 0 } : params
 end
 
-def parse_file_lines(path_name, params)
+def parse_file_lines(path_name)
   line_count = 0
 
   path_name.each_line { line_count += 1 }
-  params[:l_total] += line_count
 
   line_count
 end
 
-def parse_word_count(path_name, params)
+def parse_word_count(path_name)
   word_count = 0
 
   path_name.each_line do |line|
     word_count += line.split.size
   end
-  params[:w_total] += word_count
 
   word_count
 end
 
-def read_file_size(path_name, params)
-  file_size = File.size(path_name)
-  params[:c_total] += file_size
+def print_after_rjust(target, flag_param)
+  print target.to_s.rjust(PRINT_WIDTH) if flag_param
+end
 
-  file_size
+def parse_from_path_name(path_name)
+  {
+    l: parse_file_lines(path_name),
+    w: parse_word_count(path_name),
+    c: File.size(path_name)
+  }
+end
+
+def print_from_path_names(results, params)
+  totals = { l: 0, w: 0, c: 0 }
+
+  results.each do |path_name, result|
+    if result.instance_of?(String)
+      puts result
+    else
+      print_after_rjust(result[:l], params[:l])
+      print_after_rjust(result[:w], params[:w])
+      print_after_rjust(result[:c], params[:c])
+      print ' '
+      puts path_name
+
+      %i[l w c].each do |key|
+        totals[key] += result[key]
+      end
+    end
+  end
+
+  return unless ARGV.size >= 2
+
+  print_after_rjust(totals[:l], params[:l])
+  print_after_rjust(totals[:w], params[:w])
+  print_after_rjust(totals[:c], params[:c])
+  puts ' total'
+end
+
+def print_from_pipe_or_stdin(stdin, params)
+  print_after_rjust(stdin.join.count("\n"), params[:l])
+  print_after_rjust(stdin.size, params[:w])
+  print_after_rjust(stdin.join.bytesize, params[:c])
+  puts
 end
 
 main if __FILE__ == $PROGRAM_NAME
