40874c61c20044491dcb4459cb82cc34298e8c41
nissyi <yuta.onishi@pepabo.com>
2022-08-08 19:19:42 +0900

    Shotからscore属性を削除
    markから変換できるようにします。

--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :mark, :score
+  attr_reader :mark
 
   def initialize(mark)
     @mark = mark
-    @score = mark
+  end
+
+  def score
+    return 10 if @mark == 'X'
+
+    @mark.to_i
   end
 end
