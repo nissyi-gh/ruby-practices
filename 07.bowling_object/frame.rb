1c0e22fcb40c5979bc72349674bf111046b056e2
nissyi <yuta.onishi@pepabo.com>
2022-08-08 19:02:14 +0900

    Frameのスコア計算のテストを追加
    1~3回投球した場合をテストします。

---
+++ 07.bowling_object/frame.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative 'shot'
+
+class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def initialize(first_shot, second_shot = nil, third_shot = nil)
+    @first_shot = Shot.new(first_shot)
+    @second_shot = second_shot ? Shot.new(second_shot) : nil
+    @third_shot = third_shot ? Shot.new(third_shot) : nil
+  end
+end
--- 07.bowling_object/test/frame_test.rb
+++ 07.bowling_object/test/frame_test.rb
@@ -19,4 +19,16 @@ class TestFrame < MiniTest::Test
   def test_third_shot_score
     assert_equal 10, Frame.new(5, 5, 10).third_shot.score
   end
+
+  def test_score_when_1_time_throw
+    assert_equal 10, Frame.new(10).score
+  end
+
+  def test_score_when_2_times_throw
+    assert_equal 0, Frame.new(0, 0).score
+  end
+
+  def test_score_when_3_times_throw
+    assert_equal 18, Frame.new(5, 5, 8).score
+  end
 end
