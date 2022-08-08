17e9c01a0a5a341f1ed08abb0eba75e439d6f27b
nissyi <yuta.onishi@pepabo.com>
2022-08-08 19:15:55 +0900

    Frameのscoreに関するテスト修正
    ストライク時のテストをすることで、スコアを持つことと変換ができることを確認します。

--- 07.bowling_object/test/shot_test.rb
+++ 07.bowling_object/test/shot_test.rb
@@ -4,11 +4,11 @@ require 'minitest/autorun'
 require_relative '../shot'
 
 class TestShot < MiniTest::Test
-  def test_has_expect_score
-    assert_equal 5, Shot.new(5).score
-  end
-
   def test_has_expect_mark_when_strike
     assert_equal 'X', Shot.new('X').mark
+  end
+
+  def test_score_when_strike
+    assert_equal 10, Shot.new('X').score
   end
 end
