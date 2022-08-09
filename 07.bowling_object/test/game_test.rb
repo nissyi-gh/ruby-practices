23cbcd42285c054b84ee4557975ccce5ebfa1d54
nissyi <yuta.onishi@pepabo.com>
2022-08-09 11:05:34 +0900

    テスト再修正
    インスタンスメソッドの指定が漏れていたので修正します。

--- 07.bowling_object/test/game_test.rb
+++ 07.bowling_object/test/game_test.rb
@@ -71,31 +71,31 @@ class TestGame < MiniTest::Test
 
   def test_score_example_a
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-    assert_equal 139, Game.new
+    assert_equal 139, Game.new.score
   end
 
   def test_score_example_b
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-    assert_equal 164, Game.new
+    assert_equal 164, Game.new.score
   end
 
   def test_score_example_c
     ARGV << '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-    assert_equal 107, Game.new
+    assert_equal 107, Game.new.score
   end
 
   def test_score_example_d
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-    assert_equal 134, Game.new
+    assert_equal 134, Game.new.score
   end
 
   def test_score_example_e
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'
-    assert_equal 144, Game.new
+    assert_equal 144, Game.new.score
   end
 
   def test_score_perfect_game
     ARGV << 'X,X,X,X,X,X,X,X,X,X,X,X'
-    assert_equal 300, Game.new
+    assert_equal 300, Game.new.score
   end
 end
