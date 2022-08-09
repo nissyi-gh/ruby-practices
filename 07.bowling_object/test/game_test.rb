eb8cdf7c5e315c975cad404e854255a74f431bd1
nissyi <yuta.onishi@pepabo.com>
2022-08-09 11:02:56 +0900

    Gameのスコア計算に対するテスト方法を変更
    テストの実行ログを見やすくするために変更します。

--- 07.bowling_object/test/game_test.rb
+++ 07.bowling_object/test/game_test.rb
@@ -71,31 +71,31 @@ class TestGame < MiniTest::Test
 
   def test_score_example_a
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5'
-    assert_output("139\n") { Game.new }
+    assert_equal 139, Game.new
   end
 
   def test_score_example_b
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X'
-    assert_output("164\n") { Game.new }
+    assert_equal 164, Game.new
   end
 
   def test_score_example_c
     ARGV << '0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4'
-    assert_output("107\n") { Game.new }
+    assert_equal 107, Game.new
   end
 
   def test_score_example_d
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0'
-    assert_output("134\n") { Game.new }
+    assert_equal 134, Game.new
   end
 
   def test_score_example_e
     ARGV << '6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8'
-    assert_output("144\n") { Game.new }
+    assert_equal 144, Game.new
   end
 
   def test_score_perfect_game
     ARGV << 'X,X,X,X,X,X,X,X,X,X,X,X'
-    assert_output("300\n") { Game.new }
+    assert_equal 300, Game.new
   end
 end
